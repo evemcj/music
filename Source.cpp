6cea4f2825386ecd6f195d8d70576d1105df6683
evemcj <118966025+evemcj@users.noreply.github.com>
2022-12-16 13:38:14 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 int main() {
-	PlaySoundA(LPCSTR("a.mp3"), NULL, SND_SYNC);
+	PlaySoundA(LPCSTR("a.wav"), NULL, SND_SYNC);//use your own
 	 cout << "Press any key to swap the sequence";
     cin.ignore(1000000000, '\n');
 	return 0;
