0b2a32009563f0775032f04cb14cfcf4bb26a173
evemcj <118966025+evemcj@users.noreply.github.com>
2022-12-16 12:46:07 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,10 @@
 #include <Windows.h>
 #include <iostream>
 #include <mmsystem.h>
-
+using namespace std;
 int main() {
 	PlaySoundA(LPCSTR("a.mp3"), NULL, SND_SYNC);
+	 cout << "Press any key to swap the sequence";
+    cin.ignore(1000000000, '\n');
 	return 0;
 }
